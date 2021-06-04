512d45ad0aac3395bc92da7b5ec6956f5f8e0dac
Randy <randy_maclean@hotmail.ca>
2021-06-03 20:02:11 -0700

    Moved business object from business.js to app.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,6 +3,19 @@ import './App.css';
 import SearchBar from '../SearchBar/SearchBar';
 import BusinessList from '../BusinessList/BusinessList';
 
+const business = {
+  imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
+  name: 'MarginOtto Pizzeria',
+  address: '1010 Paddington Way',
+  city: 'Bordertown',
+  state: 'NY',
+  zipCode: '10101',
+  category: 'Italian',
+  rating: 4.5,
+  reviewCount: 90
+};
+
+
 class App extends React.Component {
   render() {
     return (
