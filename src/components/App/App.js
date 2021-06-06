f98aa64440b5b35af842116e4c428728d5f2a312
Randy <randy_maclean@hotmail.ca>
2021-06-05 18:39:14 -0700

    Added yelp.js and created yelp object

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,29 +1,31 @@
 import React from 'react';
 import './App.css';
+
 import SearchBar from '../SearchBar/SearchBar';
 import BusinessList from '../BusinessList/BusinessList';
 
-const business = {
-  imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
-  name: 'MarginOtto Pizzeria',
-  address: '1010 Paddington Way',
-  city: 'Bordertown',
-  state: 'NY',
-  zipCode: '10101',
-  category: 'Italian',
-  rating: 4.5,
-  reviewCount: 90
-};
+import Yelp from '../../util/Yelp';
 
-const businesses = [
-  business,
-  business,
-  business,
-  business,
-  business,
-  business
-];
+// const business = {
+//   imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
+//   name: 'MarginOtto Pizzeria',
+//   address: '1010 Paddington Way',
+//   city: 'Bordertown',
+//   state: 'NY',
+//   zipCode: '10101',
+//   category: 'Italian',
+//   rating: 4.5,
+//   reviewCount: 90
+// };
 
+// const businesses = [
+//   business,
+//   business,
+//   business,
+//   business,
+//   business,
+//   business
+// ];
 
 class App extends React.Component {
 
